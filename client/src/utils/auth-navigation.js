4590045f0e416895f3768e0d5ae3502688707170
YangKunYi24320314 <24320314@hdu.edu.cn>
2026-09-03 15:13:22 +0800

    增加COMMEND前端

--- client/src/utils/auth-navigation.js
+++ client/src/utils/auth-navigation.js
@@ -1,7 +1,5 @@
 export function getAuthAction(token) {
-  return token
-    ? { label: '个人中心', path: '/profile' }
-    : { label: '登录', path: '/login' }
+  return token ? { label: '个人中心', path: '/profile' } : { label: '登录', path: '/login' }
 }
 
 export function logout({ clearToken, navigate }) {
